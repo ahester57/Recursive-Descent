6b884c0b1fce331a45a0ec841dd43b144261bbd7
Austin Hester <ahester57@gmail.com>
2018-04-06 00:27:44 -0500

    setup parser

---
+++ headers/parser.h
@@ -0,0 +1,25 @@
+#ifndef PARSER_H_
+#define PARSER_H_
+
+void parser();
+void program();
+void block();
+void vars();
+void mvars();
+void expr();
+void xhelp();
+void M();
+void R();
+void stats();
+void mstat();
+void stat();
+void in();
+void out();
+void iff();
+void loop();
+void assign();
+void RO();
+void lesshelp();
+void greathelp();
+void equalhelp();
+#endif
---
+++ parser.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "scanner.h"
+#include "wordlist.h"
+#include "token.h"
+
+void
+parser() {}
+
+void
+program() {}
+
+void
+block() {}
+
+void
+vars() {}
+
+void
+mvars() {}
+
+void
+expr() {}
+
+void
+xhelp() {}
+
+void
+M() {}
+
+void
+R() {}
+
+void
+stats() {}
+
+void
+mstat() {}
+
+void
+stat() {}
+
+void
+in() {}
+
+void
+out() {}
+
+void
+iff() {}
+
+void
+loop() {}
+
+void
+assign() {}
+
+void
+RO() {}
+
+void
+lesshelp() {}
+
+void
+greathelp() {}
+
+void
+equalhelp() {}
